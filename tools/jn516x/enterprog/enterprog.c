3e017a616d016f251ac959042033634379ae9586
Röbke Geenen <robke@insanity.insaniac.net>
2018-10-17 12:01:23 +0200

    Enabled multi-mote programming, fixed Makefile bug.

--- tools/jn516x/enterprog/enterprog.c
+++ tools/jn516x/enterprog/enterprog.c
@@ -8,41 +8,87 @@
 #define BM_PROG  0b11000100
 
 int main(void){
-  struct ftdi_context *ftdi;
-  int f;
+  struct ftdi_context *ftdi_list, *ftdi;
+  struct ftdi_device_list *devlist, *cur_dev;
+  int i, res;
 
-  ftdi = ftdi_new();
-  if(!ftdi){
+  ftdi_list = ftdi_new();
+  if(!ftdi_list){
     fprintf(stderr, "Error allocating FTDI context\n");
     return EXIT_FAILURE;
   }
 
-  f = ftdi_usb_open(ftdi, 0x0403, 0x6001);
-  if((f < 0) && (f != -5)){
-    fprintf(stderr, "Error opening FTDI device: %d (%s)\n", f, ftdi_get_error_string(ftdi));
-    ftdi_deinit(ftdi);
+  res = ftdi_usb_find_all(ftdi_list, &devlist, 0x0403, 0x6001);
+  if(res < 0){
+    fprintf(stderr, "Error finding FTDI devices\n");
+    ftdi_free(ftdi_list);
     return EXIT_FAILURE;
   }
 
-  f = ftdi_set_bitmode(ftdi, BM_RESET, BITMODE_CBUS);
-  if(f < 0){
-    fprintf(stderr, "Error forcing dongle in reset mode: %d (%s)\n", f, ftdi_get_error_string(ftdi));
-    ftdi_usb_close(ftdi);
-    ftdi_deinit(ftdi);
-    return EXIT_FAILURE;
+  cur_dev = devlist;
+  i = 0;
+  while(cur_dev){
+    printf("Entering programming mode on device: %d\n", i);
+
+    ftdi = ftdi_new();
+    if(!ftdi){
+      fprintf(stderr, "Error allocating FTDI context\n");
+      continue;
+    }
+
+    res = ftdi_usb_open_dev(ftdi, cur_dev->dev);
+    if(res < 0){
+      fprintf(stderr, "Error opening FTDI device: [%d] %d (%s)\n", i, res, ftdi_get_error_string(ftdi));
+      ftdi_free(ftdi);
+      continue;
+    }
+
+    res = ftdi_set_bitmode(ftdi, BM_RESET, BITMODE_CBUS);
+    if(res < 0){
+      fprintf(stderr, "Error forcing dongle in reset mode: [%d] %d (%s)\n", i, res, ftdi_get_error_string(ftdi));
+      ftdi_usb_close(ftdi);
+      ftdi_free(ftdi);
+      continue;
+    }
+
+    res = ftdi_set_bitmode(ftdi, BM_PROG, BITMODE_CBUS);
+    if(res < 0){
+      fprintf(stderr, "Error forcing dongle in program mode: [%d] %d (%s)\n", i, res, ftdi_get_error_string(ftdi));
+      ftdi_usb_close(ftdi);
+      ftdi_free(ftdi);
+      continue;
+    }
+
+    res = ftdi_disable_bitbang(ftdi);
+    if(res < 0){
+      fprintf(stderr, "Error disabling bitbang mode: [%d] %d (%s)\n", i, res, ftdi_get_error_string(ftdi));
+      ftdi_usb_close(ftdi);
+      ftdi_free(ftdi);
+      continue;
+    }
+
+    res = usb_reset(ftdi->usb_dev);
+    if(res < 0){
+      fprintf(stderr, "Error resetting USB device: [%d]\n", i);
+      ftdi_usb_close(ftdi);
+      ftdi_free(ftdi);
+      continue;
+    }
+
+    res = ftdi_usb_close(ftdi);
+    if(res < -1){
+      fprintf(stderr, "Error closing FTDI device: [%d] %d (%s)\n", i, res, ftdi_get_error_string(ftdi));
+      ftdi_usb_close(ftdi);
+      ftdi_free(ftdi);
+      continue;
+    }
+
+    ftdi_free(ftdi);
+    cur_dev = cur_dev->next;
+    i++;
   }
 
-  f = ftdi_set_bitmode(ftdi, BM_PROG, BITMODE_CBUS);
-  if(f < 0){
-    fprintf(stderr, "Error forcing dongle in program mode: %d (%s)\n", f, ftdi_get_error_string(ftdi));
-    ftdi_usb_close(ftdi);
-    ftdi_deinit(ftdi);
-    return EXIT_FAILURE;
-  }
-
-  ftdi_disable_bitbang(ftdi);
-  usb_reset(ftdi->usb_dev);
-  ftdi_usb_close(ftdi);
-  ftdi_free(ftdi);
+  ftdi_list_free(&devlist);
+  ftdi_free(ftdi_list);
   return EXIT_SUCCESS;
 }
