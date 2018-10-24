301e0182568c59f324ca5abbda21793423aad1c2
Röbke Geenen <robke@insanity.insaniac.net>
2018-10-24 12:49:51 +0200

    Started implementation of serial command line interface.

--- implementation/node.c
+++ implementation/node.c
@@ -1,9 +1,14 @@
 #include "net/rime/polite.h"
 #include "contiki.h"
+#include "dev/serial-line.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 PROCESS(example_polite_process, "");
-AUTOSTART_PROCESSES(&example_polite_process);
+PROCESS(test_serial, "Serial line test process");
+
+//AUTOSTART_PROCESSES(&example_polite_process, &test_serial);
+AUTOSTART_PROCESSES(&test_serial);
 
 static void recv(struct polite_conn *c){
   printf("recv '%s'\n", (char*)packetbuf_dataptr());
@@ -48,3 +53,48 @@ PROCESS_THREAD(example_polite_process, ev, data){
   }
   PROCESS_END();
 }
+
+PROCESS_THREAD(test_serial, ev, data){
+  #define CMD_INVALID 0
+  #define CMD_PRICE 1
+  #define CMD_PICK 2
+
+  const unsigned char argc = 3;
+  unsigned char argi;
+  unsigned int argv[argc];
+  char *pch;
+
+  PROCESS_BEGIN();
+
+  for(;;){
+    PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message);
+    //Formats:
+    //price <product type> <new price>
+    //pick
+    argi = 0;
+    pch = strtok((char*)data, " ");
+    argv[argi] = CMD_INVALID;
+    if(strcmp(pch, "price") == 0){
+      argv[argi] = CMD_PRICE;
+    }
+    if(strcmp(pch, "pick") == 0){
+      argv[argi] = CMD_PICK;
+    }
+    argi++;
+    pch = strtok(NULL, " ");
+    while(pch && argi < argc){
+      argv[argi] = atoi(pch);
+      argi++;
+      pch = strtok(NULL, " ");
+    }
+
+    if(argv[0] == CMD_PRICE){
+      printf("Product type %u set to price %u!\n", argv[1], argv[2]);
+    }
+    if(argv[0] == CMD_PICK){
+      printf("Pick event!\n");
+    }
+  }
+  PROCESS_END();
+}
+
